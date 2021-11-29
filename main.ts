1eba0ab76bc7b66fd674708ab731f0cac42e4f13
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-11-29 20:26:30 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("" + (BMP280.temperature()))
+    OLED12864_I2C.clear()
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(BMP280.pressure() / 100)
+	
 })
 BMP280.Address(BMP280_I2C_ADDRESS.ADDR_0x76)
 BMP280.PowerOn()
+OLED12864_I2C.init()
+OLED12864_I2C.clear()
 basic.showIcon(IconNames.SmallDiamond)
+OLED12864_I2C.String("BMP-280 Demo", 25, 0, 1)
+OLED12864_I2C.hline(25, 10, 72, 1)
 basic.forever(function () {
-	
+    OLED12864_I2C.String("Temp: ", 0, 3, 1)
+    OLED12864_I2C.Number(BMP280.temperature(), 32, 3, 1)
+    OLED12864_I2C.String(" Centigrade", 44, 3, 1)
+    OLED12864_I2C.String("Pressure:  ", 0, 6, 1)
+    OLED12864_I2C.Number(BMP280.pressure() / 100, 64, 6, 1)
+    basic.pause(500)
 })
